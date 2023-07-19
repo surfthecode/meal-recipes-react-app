062e99e58fe8604a9dccc82cf0d6be8ea6421422
surfthecode <surfthecode@gmail.com>
2023-07-19 13:21:10 +0100

    finish meals details component styling

--- my-app/src/MealDetailsComponent.js
+++ my-app/src/MealDetailsComponent.js
@@ -7,43 +7,66 @@ const style = {
   details: {
     backgroundColor: "blanchedalmond",
     borderRadius: "10px",
-    padding: "5px 10px",
+    padding: "5px 15px 5px 85px",
   },
+
+  detailsWrapper: {
+    padding: "10px 0",
+    backgroundColor: "transparent",
+    transform: "translate(-75px, 0px)",
+    zIndex: "-1",
+  },
+
   title: {
     fontSize: "22px",
     color: "green",
   },
+
   image: {
     borderRadius: "50%",
-    width: 150,
-    height: 150,
+    border: "2px solid black",
+    width: 180,
+    height: 180,
   },
+
   info: {
     color: "grey",
   },
+
   label: {
     color: "black",
     fontSize: "12px",
+  },
+
+  wrapper: {
+    display: "flex",
+    alignItems: "center",
   },
 };
 
 function MealDetailsComponent(props) {
   return (
     <>
-      <div style={{}}>
+      <div style={style.wrapper}>
         <img src={props.imageSRC} alt={props.title} style={style.image}></img>
-      </div>
 
-      <div style={style.details}>
-        <h3 style={style.title}>{props.title}</h3>
-        <p style={style.info}>
-          <span style={style.label}>Area: </span>
-          {props.area}
-        </p>
-        <p style={style.info}>
-          <span style={style.label}>Category: </span>
-          {props.category}
-        </p>
+        <div style={style.detailsWrapper}>
+          <div style={style.details}>
+            <h3 style={style.title}>{props.title}</h3>
+
+            <div style={style.info}>
+              <p>
+                <span style={style.label}>Area: </span>
+                {props.area}
+              </p>
+
+              <p>
+                <span style={style.label}>Category: </span>
+                {props.category}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
