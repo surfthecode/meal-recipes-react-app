da36eb57421f6355232515ea6d1b0cf648e0728d
surfthecode <surfthecode@gmail.com>
2023-07-19 11:46:07 +0100

    change inline styleing to object stryling in meal details component

--- my-app/src/MealDetailsComponent.js
+++ my-app/src/MealDetailsComponent.js
@@ -2,6 +2,26 @@ import React from "react";
 
 // MealDetailsComponent - <h3>, <p>, <a>
 function MealDetailsComponent(props) {
+  const detailsStyle = {
+    backgroundColor: "blanchedalmond",
+    borderRadius: "10px",
+    padding: "5px 10px",
+  };
+
+  const titleStyle = {
+    fontSize: "22px",
+    color: "green",
+  };
+
+  const infoStyle = {
+    color: "grey",
+  };
+
+  const labelStyle = {
+    color: "black",
+    fontSize: "12px",
+  };
+
   return (
     <>
       <div style={{}}>
@@ -12,20 +32,15 @@ function MealDetailsComponent(props) {
           style={{ borderRadius: "50%" }}
         ></img>
       </div>
-      <div
-        style={{
-          backgroundColor: "blanchedalmond",
-          borderRadius: "10px",
-          padding: "10px 5px",
-        }}
-      >
-        <h3 style={{ fontSize: "22px", color: "green" }}>{props.title}</h3>
-        <p style={{ color: "grey" }}>
-          <span style={{ color: "black", fontSize: "12px" }}>Area: </span>
+
+      <div style={detailsStyle}>
+        <h3 style={titleStyle}>{props.title}</h3>
+        <p style={infoStyle}>
+          <span style={labelStyle}>Area: </span>
           {props.area}
         </p>
-        <p style={{ color: "grey" }}>
-          <span style={{ color: "black", fontSize: "12px" }}>Category: </span>
+        <p style={infoStyle}>
+          <span style={labelStyle}>Category: </span>
           {props.category}
         </p>
       </div>
