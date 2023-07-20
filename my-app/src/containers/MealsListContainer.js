7c9a66a0542259a7b9ef3b47a85141aac2ed056c
surfthecode <surfthecode@gmail.com>
2023-07-20 11:45:25 +0100

    create mockData and use instead of fetch; refactor meals list container to display mockData items

--- my-app/src/containers/MealsListContainer.js
+++ my-app/src/containers/MealsListContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MealDetailsComponent from "../components/MealDetailsComponent";
 import TitleComponent from "../components/TitleComponent";
+import mockData from "../mock";
 
 // MealsListContainer - contains MealDetailsComponent
 const style = {
@@ -24,32 +25,16 @@ const MealsListContainer = () => {
         <TitleComponent text="Latest Meals" />
 
         <div style={style.row}>
-          <div style={style.col}>
-            <MealDetailsComponent
-              imageSRC="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
-              title="Spicy Arrabiata Penne"
-              area="Italian"
-              category="Vegetarian"
-            />
-          </div>
-
-          <div style={style.col}>
-            <MealDetailsComponent
-              imageSRC="https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg"
-              title="Chicken Handi"
-              area="Indian"
-              category="Chicken"
-            />
-          </div>
-
-          <div style={style.col}>
-            <MealDetailsComponent
-              imageSRC="https://www.themealdb.com/images/media/meals/1550440197.jpg"
-              title="Salmon Eggs Eggs Benedict"
-              area="American"
-              category="Breakfast"
-            />
-          </div>
+          {mockData.meals.map((meal) => (
+            <div style={style.col}>
+              <MealDetailsComponent
+                imageSRC={meal.strMealThumb}
+                title={meal.strMeal}
+                area={meal.strArea}
+                category={meal.strCategory}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
